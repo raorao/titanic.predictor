899fe358b262991bb0ff3ef4434a401a643fb4ac
raorao <savinirs@gmail.com>
2016-04-28 09:43:36 -0700

    wip

---
+++ Titanic.R
@@ -0,0 +1,71 @@
+setwd('./')
+
+require('randomForest')
+require('caret')
+
+data <- read.csv('train.csv', header = T)
+
+#clean the data
+drops = c(
+  'PassengerId',
+  'Ticket',
+  'Cabin',
+  'Parch',
+  'SibSp',
+  'Name'
+)
+
+# column transformations
+data$Survived <- as.factor(data$Survived)
+levels(data$Survived) <- c("Died", "Lived")
+
+data$Pclass <- as.ordered(data$Pclass)
+
+# missing value munging
+data[is.na(data$Age), 'Age'] <- median(data$Age, na.rm = T)
+
+# engineer features
+data$HasCabin <- (data$Cabin == "")
+data$FamilySize <- data$Parch + data$SibSp
+
+data$Title <- vector(mode="character", length=nrow(data))
+data[grepl('(Miss)', data$Name), 'Title'] <- 'Miss'
+data[grepl('(Mlle\\.)', data$Name), 'Title'] <- 'Miss'
+data[grepl('(Ms\\.)', data$Name), 'Title'] <- 'Miss'
+data[grepl('(Mrs\\.)', data$Name), 'Title'] <- 'Mrs.'
+data[grepl('(Mme\\.)', data$Name), 'Title'] <- 'Mrs.'
+data[grepl('(Mr\\.)', data$Name), 'Title'] <- 'Mr.'
+data[grepl('(Master)', data$Name), 'Title'] <- 'Master'
+data[grepl('(Dr\\.)', data$Name), 'Title'] <- 'Dr.'
+data[grepl('(Rev\\.)', data$Name), 'Title'] <- 'Rev.'
+data[(data$Title == ''), 'Title'] <- 'Other'
+data$Title <- as.factor(data$Title)
+
+data <- data[,!(colnames(data) %in% drops)]
+
+
+sample <- sample(1:nrow(data), 0.7*nrow(data))
+data.train <- data[sample,]
+data.test <- data[-sample,]
+
+model = randomForest(
+  Survived ~ .,
+  data = data.train,
+  importance=TRUE
+)
+
+pdf("titanic.pdf")
+varImpPlot(model)
+dev.off()
+
+predictions <- predict(model, data.test, type="response")
+
+print(confusionMatrix(predictions, data.test$Survived, positive = "Died"))
+
+precision <- posPredValue(predictions, data.test$Survived)
+recall <- sensitivity(predictions, data.test$Survived)
+
+F1 <- (2 * precision * recall) / (precision + recall)
+
+writeLines("your F1 is...!\n")
+print(F1)
